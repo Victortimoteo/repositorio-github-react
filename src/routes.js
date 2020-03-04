5cbaeccfd1d065b2d381499f70e4d2406ad83f6a
Victortimoteo <vitortimoteo@hotmail.com>
2020-03-04 18:34:30 -0300

    Routes main e repository

---
+++ src/routes.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Main from './pages/Main';
+import Repository from './pages/Repository';
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+export default function Routes() {
+  return(
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Main} />
+        <Route path="/repository" component={Repository} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
